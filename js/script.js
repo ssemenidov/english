b2641168b178922d0272d485da5d824b1113b983
ssemenidov <23sergey03@gmail.com>
2020-06-04 21:09:06 +0800

    main + animation

--- js/script.js
+++ js/script.js
@@ -11,3 +11,30 @@ $(document).ready(function () {
     dotsClass: "preview-dots",
   });
 });
+
+let previewEndList = document.querySelectorAll(".preview-end");
+let previewNextList = document.querySelectorAll(".preview-next");
+let preview = document.querySelector(".preview");
+let header = document.querySelector(".header");
+let main = document.querySelector(".main");
+let footer = document.querySelector(".footer");
+
+function prevmain() {
+  footer.classList.add("show");
+  footer.classList.remove("hidden");
+  header.classList.add("show");
+  header.classList.remove("hidden");
+  main.classList.add("show");
+  main.classList.remove("hidden");
+  preview.classList.remove("show");
+  preview.classList.add("hidden");
+}
+previewNextList[previewEndList.length - 1].addEventListener("click", () => {
+  prevmain();
+});
+
+for (let previewend of previewEndList) {
+  previewend.addEventListener("click", () => {
+    prevmain();
+  });
+}
