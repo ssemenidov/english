aeef018dd8435a3428d106a7f161239b5149107d
ssemenidov <23sergey03@gmail.com>
2020-06-04 21:20:26 +0800

    js menu

--- js/script.js
+++ js/script.js
@@ -17,8 +17,14 @@ let previewNextList = document.querySelectorAll(".preview-next");
 let preview = document.querySelector(".preview");
 let header = document.querySelector(".header");
 let main = document.querySelector(".main");
+let test = document.querySelector(".test");
+let prof = document.querySelector(".profile");
 let footer = document.querySelector(".footer");
-
+let home_btn = document.getElementById("home");
+let test_btn = document.getElementById("test");
+let prof_btn = document.getElementById("profile");
+let open_btn = home_btn;
+let open_page = main;
 function prevmain() {
   footer.classList.add("show");
   footer.classList.remove("hidden");
@@ -32,9 +38,33 @@ function prevmain() {
 previewNextList[previewEndList.length - 1].addEventListener("click", () => {
   prevmain();
 });
-
 for (let previewend of previewEndList) {
   previewend.addEventListener("click", () => {
     prevmain();
   });
 }
+test_btn.addEventListener("click", () => {
+  open_btn = test_btn;
+  open_page.classList.remove("show");
+  open_page.classList.add("hidden");
+  open_page = test;
+  open_page.classList.add("show");
+  open_page.classList.remove("hidden");
+});
+
+home_btn.addEventListener("click", () => {
+  open_btn = home_btn;
+  open_page.classList.remove("show");
+  open_page.classList.add("hidden");
+  open_page = main;
+  open_page.classList.add("show");
+  open_page.classList.remove("hidden");
+});
+prof_btn.addEventListener("click", () => {
+  open_btn = prof_btn;
+  open_page.classList.remove("show");
+  open_page.classList.add("hidden");
+  open_page = prof;
+  open_page.classList.add("show");
+  open_page.classList.remove("hidden");
+});
